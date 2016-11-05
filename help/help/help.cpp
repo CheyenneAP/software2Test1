f9bccd333b17928797c817613e8d35562935002c
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-11-05 11:02:13 +0200

    2nd commit

--- help/help/help.cpp
+++ help/help/help.cpp
@@ -24,3 +24,68 @@ public:
 Test::Test(){
 
 }
+// default destructor 
+Test::~Test(){
+
+}
+
+void Test::writeTextFile(string link){
+	ofstream myFile(link);
+
+	if (myFile){
+		myFile << "I love Roses and chedders.\\n i love oreos.  " << endl;
+		myFile.close();
+	}
+	else{
+		cout << "File does not exist" << endl;
+	}
+
+}
+
+void Test::readTextFile(string link){
+
+	ifstream rFile;
+	rFile.open(link);
+	char output[100];
+	if (rFile && rFile.is_open())
+	{
+
+		while (!rFile.eof())
+		{
+
+			rFile >> output;
+			cout << output << " ";
+		}
+		rFile.close();
+	}
+
+
+
+}
+
+void Test::rTextFile(string link){
+	string STRING;
+
+	ifstream rFile;
+	rFile.open(link);
+
+	while (!rFile.eof)
+	{
+		getline(rFile, STRING);
+		cout << STRING;
+
+	}
+	rFile.close();
+}
+
+
+
+
+int main(){
+	Test t, r;
+	t.writeTextFile("vet.txt");
+	r.readTextFile("Chey.txt");
+
+
+
+}
