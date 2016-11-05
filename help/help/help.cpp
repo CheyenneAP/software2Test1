d20ce3c05d1be27734efa37acc98a1e576541953
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-11-05 10:58:25 +0200

    first commit

---
+++ help/help/help.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+
+class Test{
+private:
+
+
+public:
+	Test();
+	~Test();
+	void Test::writeTextFile(string);
+	void Test::readTextFile(string);
+	void Test::rTextFile(string link);
+
+};
+
+
+//  default constructor 
+Test::Test(){
+
+}
